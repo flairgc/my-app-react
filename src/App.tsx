4b59ae14f67b49bfdfbe4a2d47059bde362369fb
flairgc <flair.gc@gmail.com>
2024-04-29 19:20:16 +0000

    fix main title

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. by Flairgc
+          DEV! by Flairgc
         </p>
         <a
           className="App-link"
